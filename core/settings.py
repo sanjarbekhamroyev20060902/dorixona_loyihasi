158eacdd6c3eb122f6d4a8026a9bf60d21bb0fb2
sanjarbekhamroyev20060902 <sanjarbekhamroyev20060902@gmail.com>
2026-02-23 11:15:01 +0500

    Loyiha birinchi yuklash

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
